c44a54a248d6f4495e5cd653039ffc15403f2313
8cH9azbsFifZ <g@ziegenhain.com>
2023-04-07 11:11:04 +0200

    fixes

--- install_raspi.sh
+++ install_raspi.sh
@@ -4,12 +4,8 @@ sudo apt-get update
 sudo apt-get -y upgrade
 sudo apt-get install -y build-essential git cmake libfftw3-dev libusb-1.0-0-dev libfftw3-3 libusb-dev pkgconf libasound2-dev
 
-mkdir src
-cd src
-RUN git clone https://github.com/n1gp/librtlsdr.git
-cd librtlsdr
 mkdir build
 cd build 
-RUN cmake -DCMAKE_C_FLAGS=-fcommon  ..
-RUN make 
-RUN make install
+cmake -DCMAKE_C_FLAGS=-fcommon  ..
+make 
+make install
